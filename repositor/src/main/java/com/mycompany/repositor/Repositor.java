fc2d80b2f133c47af237887db7da6cce1444ee89
federicopunte <fpunte@alumnos.unsada.edu.ar>
2025-04-22 11:58:51 -0300

    archivo java

---
+++ repositor/src/main/java/com/mycompany/repositor/Repositor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositor;
+
+/**
+ *
+ * @author feder
+ */
+public class Repositor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
